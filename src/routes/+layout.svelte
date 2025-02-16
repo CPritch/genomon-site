ff243d0aa34407988b2f895096e320b1f4cc2ff1
CPritch <charlespritchard.work@gmail.com>
2025-02-16 19:50:30 +0000

    feat: daisyui

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,4 +3,21 @@
 	let { children } = $props();
 </script>
 
+<div class="navbar bg-base-100 shadow-sm">
+	<div class="flex-0">
+	  <a href="/" class="btn btn-ghost text-xl">Genomon</a>
+	</div>
+	<div class="flex-none">
+	  <ul class="menu menu-horizontal px-1">
+		<li>
+		  <details>
+			<summary>Tools</summary>
+			<ul class="bg-base-100 rounded-t-none p-2">
+			  <li><a href="/card-management">Card Management</a></li>
+			</ul>
+		  </details>
+		</li>
+	  </ul>
+	</div>
+  </div>
 {@render children()}
